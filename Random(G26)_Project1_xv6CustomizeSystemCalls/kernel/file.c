dd6c74fd82208da1f05b5b8af90b60eb9119300c
Pavan Kumar Bheemuni <129541339+22je0249@users.noreply.github.com>
2026-04-12 21:29:08 +0530

    Updated file.c

--- Random(G26)_Project1_xv6CustomizeSystemCalls/kernel/file.c
+++ Random(G26)_Project1_xv6CustomizeSystemCalls/kernel/file.c
@@ -1,7 +1,3 @@
-//
-// Support functions for system calls that involve file descriptors.
-//
-
 #include "types.h"
 #include "riscv.h"
 #include "defs.h"
@@ -14,167 +10,191 @@
 #include "proc.h"
 
 struct devsw devsw[NDEV];
+
 struct {
   struct spinlock lock;
   struct file file[NFILE];
 } ftable;
 
-void
-fileinit(void)
-{
+/* ===================== INIT ===================== */
+
+void fileinit(void) {
   initlock(&ftable.lock, "ftable");
 }
 
-// Allocate a file structure.
-struct file*
-filealloc(void)
-{
-  struct file *f;
+/* ===================== FILE TABLE HELPERS ===================== */
 
-  acquire(&ftable.lock);
-  for(f = ftable.file; f < ftable.file + NFILE; f++){
-    if(f->ref == 0){
+static struct file* file_find_free() {
+  for (struct file *f = ftable.file;
+       f < ftable.file + NFILE;
+       f++) {
+    if (f->ref == 0) {
       f->ref = 1;
-      release(&ftable.lock);
       return f;
     }
   }
-  release(&ftable.lock);
   return 0;
 }
 
-// Increment ref count for file f.
-struct file*
-filedup(struct file *f)
-{
+/* ===================== FILE OPERATIONS ===================== */
+
+struct file* filealloc(void) {
+  struct file *f;
+
   acquire(&ftable.lock);
-  if(f->ref < 1)
+  f = file_find_free();
+  release(&ftable.lock);
+
+  return f;
+}
+
+struct file* filedup(struct file *f) {
+  acquire(&ftable.lock);
+
+  if (f->ref < 1)
     panic("filedup");
+
   f->ref++;
+
   release(&ftable.lock);
   return f;
 }
 
-// Close file f.  (Decrement ref count, close when reaches 0.)
-void
-fileclose(struct file *f)
-{
+void fileclose(struct file *f) {
   struct file ff;
 
   acquire(&ftable.lock);
-  if(f->ref < 1)
+
+  if (f->ref < 1)
     panic("fileclose");
-  if(--f->ref > 0){
+
+  if (--f->ref > 0) {
     release(&ftable.lock);
     return;
   }
+
   ff = *f;
   f->ref = 0;
   f->type = FD_NONE;
+
   release(&ftable.lock);
 
-  if(ff.type == FD_PIPE){
+  if (ff.type == FD_PIPE) {
     pipeclose(ff.pipe, ff.writable);
-  } else if(ff.type == FD_INODE || ff.type == FD_DEVICE){
+  } 
+  else if (ff.type == FD_INODE || ff.type == FD_DEVICE) {
     begin_op();
     iput(ff.ip);
     end_op();
   }
 }
 
-// Get metadata about file f.
-// addr is a user virtual address, pointing to a struct stat.
-int
-filestat(struct file *f, uint64 addr)
-{
+/* ===================== FILE METADATA ===================== */
+
+int filestat(struct file *f, uint64 addr) {
   struct proc *p = myproc();
   struct stat st;
-  
-  if(f->type == FD_INODE || f->type == FD_DEVICE){
-    ilock(f->ip);
-    stati(f->ip, &st);
-    iunlock(f->ip);
-    if(copyout(p->pagetable, addr, (char *)&st, sizeof(st)) < 0)
-      return -1;
-    return 0;
-  }
-  return -1;
-}
 
-// Read from file f.
-// addr is a user virtual address.
-int
-fileread(struct file *f, uint64 addr, int n)
-{
-  int r = 0;
-
-  if(f->readable == 0)
+  if (f->type != FD_INODE && f->type != FD_DEVICE)
     return -1;
 
-  if(f->type == FD_PIPE){
-    r = piperead(f->pipe, addr, n);
-  } else if(f->type == FD_DEVICE){
-    if(f->major < 0 || f->major >= NDEV || !devsw[f->major].read)
-      return -1;
-    r = devsw[f->major].read(1, addr, n);
-  } else if(f->type == FD_INODE){
-    ilock(f->ip);
-    if((r = readi(f->ip, 1, addr, f->off, n)) > 0)
-      f->off += r;
-    iunlock(f->ip);
-  } else {
-    panic("fileread");
-  }
+  ilock(f->ip);
+  stati(f->ip, &st);
+  iunlock(f->ip);
+
+  return (copyout(p->pagetable, addr,
+                  (char *)&st, sizeof(st)) < 0) ? -1 : 0;
+}
+
+/* ===================== READ ===================== */
+
+static int fileread_pipe(struct file *f, uint64 addr, int n) {
+  return piperead(f->pipe, addr, n);
+}
+
+static int fileread_device(struct file *f, uint64 addr, int n) {
+  if (f->major < 0 || f->major >= NDEV || !devsw[f->major].read)
+    return -1;
+  return devsw[f->major].read(1, addr, n);
+}
+
+static int fileread_inode(struct file *f, uint64 addr, int n) {
+  int r;
+
+  ilock(f->ip);
+  r = readi(f->ip, 1, addr, f->off, n);
+  if (r > 0)
+    f->off += r;
+  iunlock(f->ip);
 
   return r;
 }
 
-// Write to file f.
-// addr is a user virtual address.
-int
-filewrite(struct file *f, uint64 addr, int n)
-{
-  int r, ret = 0;
-
-  if(f->writable == 0)
+int fileread(struct file *f, uint64 addr, int n) {
+  if (!f->readable)
     return -1;
 
-  if(f->type == FD_PIPE){
-    ret = pipewrite(f->pipe, addr, n);
-  } else if(f->type == FD_DEVICE){
-    if(f->major < 0 || f->major >= NDEV || !devsw[f->major].write)
-      return -1;
-    ret = devsw[f->major].write(1, addr, n);
-  } else if(f->type == FD_INODE){
-    // write a few blocks at a time to avoid exceeding
-    // the maximum log transaction size, including
-    // i-node, indirect block, allocation blocks,
-    // and 2 blocks of slop for non-aligned writes.
-    int max = ((MAXOPBLOCKS-1-1-2) / 2) * BSIZE;
-    int i = 0;
-    while(i < n){
-      int n1 = n - i;
-      if(n1 > max)
-        n1 = max;
-
-      begin_op();
-      ilock(f->ip);
-      if ((r = writei(f->ip, 1, addr + i, f->off, n1)) > 0)
-        f->off += r;
-      iunlock(f->ip);
-      end_op();
-
-      if(r != n1){
-        // error from writei
-        break;
-      }
-      i += r;
-    }
-    ret = (i == n ? n : -1);
-  } else {
-    panic("filewrite");
+  switch (f->type) {
+    case FD_PIPE:   return fileread_pipe(f, addr, n);
+    case FD_DEVICE: return fileread_device(f, addr, n);
+    case FD_INODE:  return fileread_inode(f, addr, n);
+    default:        panic("fileread");
   }
 
-  return ret;
+  return -1;
 }
 
+/* ===================== WRITE ===================== */
+
+static int filewrite_device(struct file *f, uint64 addr, int n) {
+  if (f->major < 0 || f->major >= NDEV || !devsw[f->major].write)
+    return -1;
+  return devsw[f->major].write(1, addr, n);
+}
+
+static int filewrite_pipe(struct file *f, uint64 addr, int n) {
+  return pipewrite(f->pipe, addr, n);
+}
+
+static int filewrite_inode(struct file *f, uint64 addr, int n) {
+  int i = 0, r;
+
+  int max = ((MAXOPBLOCKS - 1 - 1 - 2) / 2) * BSIZE;
+
+  while (i < n) {
+    int n1 = n - i;
+    if (n1 > max)
+      n1 = max;
+
+    begin_op();
+    ilock(f->ip);
+
+    r = writei(f->ip, 1, addr + i, f->off, n1);
+    if (r > 0)
+      f->off += r;
+
+    iunlock(f->ip);
+    end_op();
+
+    if (r != n1)
+      break;
+
+    i += r;
+  }
+
+  return (i == n) ? n : -1;
+}
+
+int filewrite(struct file *f, uint64 addr, int n) {
+  if (!f->writable)
+    return -1;
+
+  switch (f->type) {
+    case FD_PIPE:   return filewrite_pipe(f, addr, n);
+    case FD_DEVICE: return filewrite_device(f, addr, n);
+    case FD_INODE:  return filewrite_inode(f, addr, n);
+    default:        panic("filewrite");
+  }
+
+  return -1;
+}
